4901f7ef4d3ccbbc438ad2797042e33e57d2e33c
sandipthapa99 <sandipthapa99@users.noreply.github.com>
2020-01-30 20:29:19 +0545

    API trial 1

--- LearnAndQuiz/courses/urls.py
+++ LearnAndQuiz/courses/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -5,4 +6,8 @@ app_name="courses"
 
 urlpatterns = [
 	path('courses/', views.coursePage, name = 'courses'),
+	path('api/', views.read_api, name = 'read_api'),
+	path('api/update/<int:pk>/', views.update_api, name = 'update_api'),
+	path('api/delete/<int:pk>/', views.delete_data, name = 'delete_data'),
+
 ]
